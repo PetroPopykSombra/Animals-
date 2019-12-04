427ac9cfb4b5b24e2cbe13291b62210c85c2df71
Petro Popyk <petro.popyk@sombrainc.com>
2019-12-04 19:22:22 +0200

    changes.

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -25,13 +25,15 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: false,
-               browsers: ['ChromeNoSandbox'],
-               customLaunchers: {
-                 ChromeNoSandbox: {
-                   base: 'Chrome',
-                   flags: ['--no-sandbox']
-                 }
-               },
+               browsers: ['Chrome'],
+               browserDisconnectTimeout: 10000,
+               browserDisconnectTolerance: 3,
+               browserNoActivityTimeout: 60000,
+               flags: [
+                 '--disable-web-security',
+                 '--disable-gpu',
+                 '--no-sandbox'
+               ],
     singleRun: true
   });
 };
